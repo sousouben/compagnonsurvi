71a7c0881286aa6161dbe243e98c03ca8ef21d35
SOUSOUBENH <75803460+sousouben@users.noreply.github.com>
2025-06-18 15:30:55 +0200

    suppression capacitor

--- android/app/src/main/java/com/example/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.compagnondesurvie.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.compagnondesurvie.app',
-  appName: 'compagnon-de-survie',
-  webDir: 'dist'
-};
-
-export default config;
